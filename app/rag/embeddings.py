15ef0a92a3fd70a4cb57b4e18f73b400e78a3f01
Benjamin Carreón <bcarreon25@alumnos.uaq.mx>
2026-09-18 02:14:33 -0600

    added the some requirements missed to the instalation in requeriments files, added the option to let the agent get context about the lastest prompts

--- app/rag/embeddings.py
+++ app/rag/embeddings.py
@@ -1,6 +1,9 @@
 from langchain_ollama import OllamaEmbeddings
 
-embeddings=OllamaEmbeddings(
+from app.config.settings import settings
 
-    model="nomic-embed-text"
+
+embeddings = OllamaEmbeddings(
+    model=settings.OLLAMA_EMBEDDING_MODEL,
+    base_url=settings.OLLAMA_BASE_URL,
 )
